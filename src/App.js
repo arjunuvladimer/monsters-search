3cce9f812264366f07aca285c9791ec507ab9995
arjunuvladimer <arjunuvladimer@gmail.com>
2014-08-12 05:30:00 +0530

    Card List and Card Container Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Monsters Rolodex</h1>
+      <div className='card-list'>
+        <div className='card-container'>
+          <img alt='monster' src='https://robohash.org/1?set=set2&size=180x180'/>
+          <h2>Monster Name</h2>
+          <p>Monster Email</p>
+        </div>
+      </div>
     </div>
   );
 }
