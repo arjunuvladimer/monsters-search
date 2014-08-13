1f63eb4eda2e097be2c9b5c9f9c7c6ca8dac1748
arjunuvladimer <arjunuvladimer@gmail.com>
2014-08-13 05:30:00 +0530

    Setting the state using the API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Monsters Rolodex</h1>
-      <div className='card-list'>
-        <div className='card-container'>
-          <img alt='monster' src='https://robohash.org/1?set=set2&size=180x180'/>
-          <h2>Monster Name</h2>
-          <p>Monster Email</p>
-        </div>
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      monsters:[]
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+  render(){
+    const {monsters} = this.state
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <div className='card-list'>
+         {monsters.map(monster => (
+          <div className='card-container'>
+              <img alt='monster' src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+              <h2>{monster.name}</h2>
+              <p>{monster.email}</p>
+          </div>
+         ))}
+         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
