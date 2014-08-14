fed015f9087aa39a351d5d78237e1e1c3366f7c1
arjunuvladimer <arjunuvladimer@gmail.com>
2014-08-14 05:30:00 +0530

    Updates

---
+++ precache-manifest.67f5a1fed0fc20fa2a785d8b356ed76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b57f7675df65d16e1ade76f9c43787",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dabc70eea398b4b2fcf6",
+    "url": "/monsters-rolodex/static/css/main.f6749cd9.chunk.css"
+  },
+  {
+    "revision": "9da6b7882eab234860ae",
+    "url": "/monsters-rolodex/static/js/2.7721ac50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7721ac50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabc70eea398b4b2fcf6",
+    "url": "/monsters-rolodex/static/js/main.1276e0ca.chunk.js"
+  },
+  {
+    "revision": "74fe9f909a110daa43a5",
+    "url": "/monsters-rolodex/static/js/runtime-main.f0774556.js"
+  }
+]);
